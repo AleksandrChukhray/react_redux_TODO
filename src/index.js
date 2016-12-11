ae7edc92e56f680ca414a912e285657ffc4958a0
Alex <zimmalex6@gmail.com>
2016-12-11 19:08:52 +0200

    todo app complated commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import App from './containers/App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
+let store = createStore(todoApp)
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
